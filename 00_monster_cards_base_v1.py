aa5a0251e46614f0401e748cf6d0b246eae0d1e3
OnlyNoodles <balakrishn4@middleton.school.nz>
2024-05-16 15:14:34 +1200

    cdcfed

--- 00_monster_cards_base_v1.py
+++ 00_monster_cards_base_v1.py
@@ -1,9 +1,9 @@
 """00_monster_cards_base_v1.
 This is the first version of the base code for Monster Game.
 For each of the versions for these, I will add finished components to
-the option's menu. The first component I have added here, along with
-implementing an option's menu, is the welcome message and name
-function (01_welcome_message_and_name_v2)."""
+the option's menu. In this version of the base code, I have added the
+Option's Menu and the welcome message to the program. For now, the
+Option's Menu is empty besides an option to exit the program."""
 
 
 import easygui
---
+++ 01_yes_no_checker_v1.py
@@ -0,0 +1,21 @@
+"""01_yes_no_checker_v1.
+This is the first version of the Yes/No Checker.
+It is the first component that the user will experience
+in the program as it will ask the user of they have used
+this program before and if not, it will display the
+instructions component."""
+
+# Ask the user if they have played before
+show_instructions = input("Have you played this game before? :")
+
+# If they say yes, output 'Program Continues'
+if show_instructions == "yes":
+    print("program continues")
+
+# If they say no, output 'Display Instructions'
+elif show_instructions == "no":
+    print("Display instructions")
+
+# Otherwise - show error
+else:
+    print("Please answer 'yes' or 'no'")
